1451fdee708522eef679feb276d3a5a2a7ca78af
Pedro <zpfsilva@gmail.com>
2016-09-22 18:25:26 -0300

    teste banco de dados

--- easports/src/main/java/br/com/easports/domain/DAO.java
+++ easports/src/main/java/br/com/easports/domain/DAO.java
@@ -2,14 +2,16 @@ package br.com.easports.domain;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DAO {
+	
 	public static Connection getConnection() {
-
+		
 		Connection conn = null;
 		final String caminho = System.getProperty("user.dir");
-
+	
 		try {
 			conn = DriverManager.getConnection("jdbc:sqlite:" + caminho + "/src/main/resources/br/com/easports/bd.db");
 			System.out.println("dentro");
@@ -17,13 +19,36 @@ public class DAO {
 			System.out.println("erro");
 			e.printStackTrace();
 		}
-
 		return conn;
 	}
+	
+	public static void incluir() {
+		
+		PreparedStatement stmt= null;
+		Connection conn = null;
+		
+		String sql = "INSERT INTO TB_PESSOA (ID_PESSOA) VALUES (?)";
+		
+		try {
+			conn = getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, "1");
+			stmt.executeUpdate();
+			
+			
+		} catch (Exception e) {
+System.out.println("erro ao incluir");		}
+		
+	}
+	
 
+	
+	
+	
 	public static void main(final String[] args) {
-
-		DAO.getConnection();
+		
+		incluir();
+		
 
 	}
 }
