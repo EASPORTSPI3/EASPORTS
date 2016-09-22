63c01569a42aaaba882f8f40bacff5df0a0119a1
Pedro <zpfsilva@gmail.com>
2016-09-21 23:28:32 -0300

    criacao da classe DAO, atualizao do POM, insercao do banco em /resources

--- easports/src/main/java/br/com/easports/domain/DAO.java
+++ easports/src/main/java/br/com/easports/domain/DAO.java
@@ -1,5 +1,31 @@
 package br.com.easports.domain;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DAO {
+
+	public static Connection getConnection() {
+
+		Connection conn = null;
+		String caminho = System.getProperty("user.dir");
+
+		try {
+			conn = DriverManager.getConnection("jdbc:sqlite:" + caminho + "/src/main/resources/br/com/easports/bd.db");
+			System.out.println("dentro");
+		} catch (SQLException e) {
+			System.out.println("erro");
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
+	public static void main(String[] args) {
+
+		DAO.getConnection();
+
+	}
 
 }
--- easports/src/main/java/br/com/easports/presentation/Telas.java
+++ easports/src/main/java/br/com/easports/presentation/Telas.java
@@ -1,5 +1,7 @@
 package br.com.easports.presentation;
 
 public class Telas {
-
+public static void main(String[] args) {
+	
+}
 }
