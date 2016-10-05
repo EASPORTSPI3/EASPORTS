2c962e1b17f71e85f99c45894c5d37356732aa40
Diego Fernandes <diegofernandes88@hotmail.com>
2016-10-05 11:47:41 -0300

    Diretório do Banco de Dados movido para a pasta raiz do Eclipse

--- easports/src/br/com/easports/persistence/DAO.java
+++ easports/src/br/com/easports/persistence/DAO.java
@@ -4,10 +4,11 @@ import java.sql.*;
 
 public class DAO {
 
+	//private static final String DIRETORIO = "\\WebContent\\WEB-INF\\lib\\easports.db";
 	private static final String DRIVER = "org.sqlite.JDBC";
 	private static final String DATABASE = "jdbc:sqlite:";
 	private static final String URL = System.getProperty("user.dir");;
-	private static final String DIRETORIO = "\\WebContent\\WEB-INF\\lib\\easports.db";
+	private static final String BANCO = "\\easports.db";
 
 	protected Connection con;
 	protected PreparedStatement stmt;
@@ -18,8 +19,14 @@ public class DAO {
 		Class.forName(DRIVER);
 
 		if (con == null) {
+			
+			//COPIAR O ARQUIVO DO BANCO DE DADOS, LOCALIZADO NA PASTA RAIZ DO PROJETO, PARA A PASTA RAIZ DO ECLIPSE
+			
+			System.out.println(DATABASE + URL + BANCO);
+			
+			con = DriverManager.getConnection(DATABASE + URL + BANCO);
 
-			con = DriverManager.getConnection(DATABASE +"C:\\Users\\cquadros\\workspace\\EASPORTS\\easports\\WebContent\\WEB-INF\\lib\\easports.db");
+			//con = DriverManager.getConnection(DATABASE +"C:\\Users\\cquadros\\workspace\\EASPORTS\\easports\\WebContent\\WEB-INF\\lib\\easports.db");
 			
 		}
 	}
